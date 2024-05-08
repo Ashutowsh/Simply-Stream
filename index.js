6529ada9f412c11ac0a8b57b11de61dab5ad3979
Ashutosh <Ashutosh@LAPTOP-N00785I6>
2024-05-08 14:54:32 +0530

    Video Segmentation completed

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import multer from "multer";
 import { v4 as uuidv4 } from "uuid";
 import path from "path";
+import fs from "fs";
+import { exec } from "child_process";
+// import { stderr, stdout } from "process";
 const app = express();
 
 const storage = multer.diskStorage({
@@ -47,10 +50,39 @@ app.get("/", (req, res) => {
 });
 
 app.post("/upload", upload.single("file"), function (req, res) {
-  console.log("file uploaded");
-  res.send({
-    message: "Video successfully uploaded.",
-    success: true,
+  // console.log("file uploaded");
+  const videoId = uuidv4();
+  const videoPath = req.file.path;
+  const outputPath = `./uploads/nature/${videoId}`;
+  const hlsPath = `${outputPath}/index.m3u8`;
+  console.log(hlsPath);
+
+  if (!fs.existsSync(outputPath)) {
+    fs.mkdirSync(outputPath, { recursive: true });
+  }
+
+  // console.log(outputPath);
+  // console.log(videoPath);
+  // console.log(hlsPath);
+
+  // ffmpeg command
+  const ffmpegCommand = `ffmpeg -i ${videoPath} -codec:v libx264 -codec:a aac -hls_time 10 -hls_playlist_type vod -hls_segment_filename "${outputPath}/segment%03d.ts" -start_number 0 ${hlsPath}`;
+
+  exec(ffmpegCommand, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`Exec Error : ${error}`);
+    }
+
+    console.log(`stdout : ${stdout}`);
+    console.log(`stderr : ${stderr}`);
+
+    const videoUrl = `http://localhost:${port}/uploads/nature/${videoId}/index.m3u8`;
+
+    res.json({
+      message: "Video successfully converted to hls.",
+      videoUrl: videoUrl,
+      videoId: videoId,
+    });
   });
 });
 
